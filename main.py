7d57aa908effe315dd7006dfd7139f8ea6bc2ab7
Federico Gorini <gorinifederico10@gmail.com>
2026-04-23 01:08:11 -0300

    fix descarga mobile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from starlette.background import BackgroundTask
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+from starlette.background import BackgroundTask
 import yt_dlp
 import os
 import uuid
@@ -19,29 +19,55 @@ app.add_middleware(
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
+@app.post("/info")
+async def get_info(data: dict):
+    url = data.get("url")
+    ydl_opts = {"quiet": True, "skip_download": True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return JSONResponse({
+            "title": info.get("title", "Video"),
+            "thumbnail": info.get("thumbnail", ""),
+            "duration": info.get("duration", 0),
+        })
+
 @app.post("/download")
 async def download_video(data: dict):
     url = data.get("url")
+    formato = data.get("formato", "mp4")
     file_id = str(uuid.uuid4())
-    outtmpl = os.path.join(DOWNLOAD_FOLDER, f"{file_id}.%(ext)s")
 
-    ydl_opts = {
-        "outtmpl": outtmpl,
-        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best",
-        "merge_output_format": "mp4",
-    }
+    if formato == "mp3":
+        outtmpl = os.path.join(DOWNLOAD_FOLDER, f"{file_id}.%(ext)s")
+        ydl_opts = {
+            "outtmpl": outtmpl,
+            "format": "bestaudio/best",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "320",
+            }],
+        }
+        ext = "mp3"
+    else:
+        outtmpl = os.path.join(DOWNLOAD_FOLDER, f"{file_id}.%(ext)s")
+        ydl_opts = {
+            "outtmpl": outtmpl,
+            "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio/best",
+            "merge_output_format": "mp4",
+        }
+        ext = "mp4"
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
     files = glob.glob(os.path.join(DOWNLOAD_FOLDER, f"{file_id}.*"))
     filepath = files[0]
+    filename = f"video.{ext}"
 
     background = BackgroundTask(os.remove, filepath)
-    response = FileResponse(filepath, filename="video.mp4", background=background)
-    return response
+    return FileResponse(filepath, filename=filename, background=background)
 
 @app.get("/")
 def root():
     return FileResponse("index.html")
-    
